f8551d861ad8399cc39c82ec761a69feca836c7f
Zaida Guadalupe <zaida@secret-source.eu>
2021-11-29 20:49:25 +0000

    Renaming interface as Service

--- app/Adapters/RestWeatherAdapter.php
+++ app/Adapters/RestWeatherAdapter.php
@@ -2,17 +2,17 @@
 
 namespace App\Adapters;
 
-use App\Contracts\WeatherInterface;
+use App\Contracts\WeatherService;
 use Illuminate\Support\Facades\Http;
 use PhpParser\Node\Expr\Cast\Object_;
 
-class RestWeatherAdapter implements WeatherInterface {
+class RestWeatherAdapter implements WeatherService {
 
     private $endoint;
     
     public function __construct()
     {
-        $this->endpoint = 'https://restcountries.com/v3.1/';
+        $this->endpoint = config('services.weather.endpoint');
     }
 
     public function getRadiationUVdaily(string $name) : array
